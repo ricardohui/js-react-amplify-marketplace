3add6bf3505d790195052e01f0d32fe43921a38e
Ricardo Hui <eehaha@gmail.com>
2019-05-09 06:57:17 +0800

    add amplify backend code

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { API } from "aws-amplify";
 // import { Notification, Message } from "element-react";
 import StripeCheckout from "react-stripe-checkout";
 
@@ -8,8 +9,27 @@ const stripeConfig = {
 };
 
 const PayButton = ({ product, user }) => {
+  const handleCharge = async token => {
+    try {
+      const result = await API.post("orderlambda", "/items", {
+        body: {
+          token: token,
+          charge: {
+            currency: stripeConfig.currency,
+            amount: product.price,
+            description: product.description
+          }
+        }
+      });
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <StripeCheckout
+      token={handleCharge}
       email={user.attributes.email}
       name={product.description}
       amount={product.price}
